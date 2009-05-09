74cedac02036d0e5f3b51eb1a7526717f7fcf621
Christian Weiske <cweiske@cweiske.de>
2009-05-09 13:29:53 +0200

    initial checking

---
+++ run_gettext.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+export TEST_PHP_EXECUTABLE=/usr/local/bin/php53
+phpsrc=/home/cweiske/Dev/cvs/php/testfest/php-src
+
+ext=gettext
+extdir=$phpsrc/ext/$ext
+testdir=tests/$ext
+
+#cleanup
+rm covdata-$ext.info
+rm $extdir/$ext.gcda
+rm -r htmlcoverage_$ext
+
+#run
+$TEST_PHP_EXECUTABLE\
+ $phpsrc/run-tests.php\
+ $testdir/gettext_basic-enus.phpt
+
+#html
+lcov --directory $extdir -c -o covdata-$ext.info
+genhtml covdata-$ext.info -o htmlcoverage_$ext
